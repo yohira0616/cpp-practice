31113e939989e2a9f32a141d38458a8ff84fa89e
yukito ohira <yukito.sys.so@gmail.com>
2017-07-29 19:10:23 +0900

    演習1.3.4

---
+++ ch01-04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    char ch;
+    float f;
+    double d;
+
+    ch = 'X';
+    f = 100.123;
+    d = 123.003;
+
+    printf("ch is %c,", ch);
+    printf("f is %f,", f);
+    printf("d is %f", d);
+    printf("\n");
+
+    return 0;
+}
