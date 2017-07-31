401f51f67a9f7c8337dfc4ee49120b4d30355b06
yukito ohira <yukito.sys.so@gmail.com>
2017-07-31 21:20:02 +0900

    演習2-4-3

--- prac2-4-3.c
+++ prac2-4-3.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-/*
- * 素数判定プログラム
- */
 int main(void) {
     int num = 0;
     int i;
@@ -16,13 +13,13 @@ int main(void) {
     is_prime = 1;
     for (i = 2; i <= num / 2; i = i + 1) {
         if ((num % i) == 0) {
+            printf("%d\n", i);
             is_prime = 0;
         }
-        if (is_prime == 1) {
-            printf("素数です");
-        } else {
-            printf("素数ではない");
-        }
+    }
+
+    if (is_prime == 1) {
+        printf("素数です");
     }
 
     return 0;
